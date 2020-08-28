1206c0053588a18660f6ba6efb29f7dbca99a2d1
LiamHooks <lrh5428@psu.edu>
2020-08-28 16:50:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+celsiusInput = float(input("Enter temperature in celsius: "))
+convert = float((celsiusInput * 1.8) + 32)
+print(f"{celsiusInput}° in Celsius is equivalent to {convert}° Fahrenheit.")
